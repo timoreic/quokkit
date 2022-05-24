bd158e68809c2173f094fbdcb4b272b6d619b3e1
Timo <t.reichelt@live.de>
2022-05-24 16:30:38 +0800

    created and tested custom user model

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd-party apps
+
+    # Local
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +131,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
