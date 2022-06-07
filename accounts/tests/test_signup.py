80d55120f719a81bb7880cab8797bdb6e38852ee
Timo <t.reichelt@live.de>
2022-06-07 10:56:22 +0800

    Changed username authentication to email authentication and edited tests for signup

--- accounts/tests/test_signup.py
+++ accounts/tests/test_signup.py
@@ -1,23 +1,26 @@
-from django.test import SimpleTestCase
+from django.test import TestCase
 from django.urls import reverse, resolve
-from accounts.views import SignupPageView
+from django.contrib.auth import get_user_model
 
 
-class SignupPageTests(SimpleTestCase):
+class SignupPageTests(TestCase):
 
     def setUp(self):
-        url = reverse('signup')
+        url = reverse('account_signup')
         self.response = self.client.get(url)
 
-    def test_home_status_code(self):
+    def test_signup_status_code(self):
         self.assertEqual(self.response.status_code, 200)
 
-    def test_home_template(self):
-        self.assertTemplateUsed(self.response, 'registration/signup.html')
+    def test_signup_template(self):
+        self.assertTemplateUsed(self.response, 'account/signup.html')
 
-    def test_home_url_resolves_signuppageview(self):
-        view = resolve('/accounts/signup/')
-        self.assertEqual(
-            view.func.__name__,
-            SignupPageView.as_view().__name__
+    def test_signup_form(self):
+        new_user = get_user_model().objects.create_user(
+            'newuser',
+            'newuser@email.com'
         )
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, 'newuser')
+        self.assertEqual(
+            get_user_model().objects.all()[0].email, 'newuser@email.com')
